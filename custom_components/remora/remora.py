d00b440f6be4fe30df9c23d9878b495a2912dc01
Laurent Lemercier <22855554+laurentlemercier@users.noreply.github.com>
2024-10-27 19:29:40 +0000

    Teleinfo missing

--- custom_components/remora/remora.py
+++ custom_components/remora/remora.py
@@ -39,10 +39,10 @@ class RemoraDevice:
     async def async_get_TeleInfo(self) -> dict:
         """Get the status from Remora TeleInfo and return it as a dict."""
         teleinfo = await self._remora.getTeleInfo()
-        if teleinfo!=None
-           return teleinfo
-        else
-           return NUL_TELEINFO
+        if teleinfo != None:
+            return teleinfo
+        else:
+            return NUL_TELEINFO
         # return await self._remora.getTeleInfo()!==None
 
     @Throttle(MIN_TIME_BETWEEN_UPDATES_REMORA_SENSOR)
